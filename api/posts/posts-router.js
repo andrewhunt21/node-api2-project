2c79d85ba687952bcb1d9ebd82eeb6413af02225
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-11 16:10:51 -0800

    can get posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,6 +3,17 @@ const express = require('express')
 const router = express.Router()
 const Post = require('./posts-model')
 
-
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            })
+        })
+})
 
 module.exports = router
