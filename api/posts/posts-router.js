08814ef4fbf3a5588e5773ccddca03cc38e8726b
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-11 16:18:31 -0800

    can get by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const Post = require('./posts-model')
 
+//GET
 router.get('/', (req, res) => {
     Post.find(req.query)
         .then(posts => {
@@ -10,9 +11,22 @@ router.get('/', (req, res) => {
         })
         .catch(error => {
             console.log(error)
-            res.status(500).json({
-                message: "The posts information could not be retrieved"
-            })
+            res.status(500).json({ message: "The posts information could not be retrieved" })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(posts => {
+            if (posts) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ message: "The post information could not be retrieved" })
         })
 })
 
